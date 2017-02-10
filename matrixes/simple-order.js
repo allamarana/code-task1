9ec2ea6e5513dedf331ecf3a4d660ac423ced387
Liudmyla Troianovska <allamarana.t@gmail.com>
2017-02-10 13:59:14 +0200

    snake array

--- matrixes/simple-order.js
+++ matrixes/simple-order.js
@@ -3,7 +3,12 @@ function createMatrix(n, m) {
 	for(var i = 0; i < n; i++) {
 		matrix[i] = [];
 		for(var j = 0; j < m; j++) {
-			matrix[i][j] = j + 1 + i * n;
+			var v = j + 1 + i * n
+			if (i % 2 == 0) {
+				matrix[i][j] = v;
+			} else {
+				matrix[i][m - j - 1] =  v;
+			}
 		}
 	}
 	return matrix;
