33447e50a5e5a533e1469440222a98c3ae44c6c7
allamarana <allamarana.t@gmail.com>
2017-07-12 01:54:33 +0300

    bad Canvas

--- canvas.js
+++ canvas.js
@@ -1,2 +1,8 @@
 var c = document.getElementById('firstCanvas');
 var ctx = c.getContext('2d');
+c.beginPath();
+c.moveTo(10, 50);
+c.lineTo(40, 90);
+c.lineTo(120, 140);
+c.lineTo(300, 338);
+c.stroke();
