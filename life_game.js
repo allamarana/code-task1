1a38571fbb469e3a5006867b92f2f2d075022746
allamarana <allamarana.t@gmail.com>
2017-07-18 01:05:00 +0300

    work on life game continues

--- life_game.js
+++ life_game.js
@@ -1,44 +1,41 @@
-var c = document.getElementById('firstCanvas');
-var ctx = c.getContext('2d');
-function verticalParallel(n){
-	ctx.beginPath();
-	for(var i = 0; i < n; i++){
-		var x = i * (c.width / n);
-		ctx.moveTo(x, 0);
-		ctx.lineTo(x, c.height);
-		
-}
-ctx.stroke();
-}
+/*var c = document.getElementById('firstCanvas');
+var ctx = c.getContext('2d');*/
 
-function horizontalParallel(m){
-	ctx.beginPath();
-	for (var j = 0; j < m; j++){
-		var y = j * (c.height / m);
-		ctx.moveTo(0, y);
-		ctx.lineTo(c.width, y);
+function countNeighbours(matrix, x, y) {
+	var count = 0;
+	if(matrix[x + 1] && matrix[x + 1][y] === 1){
+		count++;
 	}
-	ctx.stroke();
-}
-
-function drawMatrix(matrix){
-	verticalParallel(matrix.length);
-	horizontalParallel(matrix[0].length);
-	for(var i = 0; i < matrix.length; i++){
-		for(var j = 0; j < matrix[0].length; j++){
-			if(matrix[i][j] === 1){
-				ctx.fillStyle = 'black';
-				ctx.fillRect(i * (c.width / matrix.length), j * (c.height / matrix[0].length), 
-					c.width / matrix.length, c.height / matrix[0].length);
-			}
-		}
-	}	
-}
-
-function life(matrix){
-	drawMatrix(matrix);
-	for(var i = 0; i < matrix.length; i++){
-		for(var j = 0; j < matrix[0].length; j++)
+	if(matrix[x + 1][y - 1] === 1){
+		count++;
 	}
+	if(matrix[x - 1][y] === 1){
+		count++;
+	}
+	if(matrix[x - 1][y - 1] === 1){
+		count++;
+	}
+	if(matrix[x][y + 1] === 1){
+		count++;
+	}
+	if(matrix[x][y - 1] === 1){
+		count++;
+	}
+	return count;
 }
 
+/*function nextGeneration(matrix) {
+	const newMatrix = createMatrix(matrix.length, matrix[0].length);
+
+	return matrix;
+}
+
+
+var matrix = createMatrix(...);
+setInterval(function() {
+	drawMatrix();
+	matrix = nextGeneration(matrix);
+}, 1000);*/
+
+
+console.log(countNeighbours([[1, 0, 1], [0, 0, 0], [1, 0, 0]], 2, 2))
