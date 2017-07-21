0884a596d6f141f75a08e4ff95573fa7f03d62a7
allamarana <allamarana.t@gmail.com>
2017-07-21 15:06:18 +0300

    snake directions added

--- snake.js
+++ snake.js
@@ -10,15 +10,56 @@ function applySnakeToMatrix(snake, matrix){
 
 }
 
+function snakeDirection(snake, direction, matrix){
+	var x, y;
+	if(direction === 'right'){
+		x = snake[0][0];
+		y = snake[0][1];
+		matrix[x][y] = 0
+		snake.push([snake[2][0] + 1, snake[2][1]]);
+	}
+	if(direction === 'left'){
+		x = snake[2][0];
+		y = snake[2][1];
+		matrix[x][y] = 0
+		snake.push([snake[0][0] - 1, snake[0][1]]);
+	}
+	if(direction === 'down-left'){
+		x = snake[2][0];
+		y = snake[2][1];
+		matrix[x][y] = 0
+		snake.push([snake[0][0], snake[0][1] + 1]);
+	}
+	if(direction === 'up-left'){
+		x = snake[2][0];
+		y = snake[2][1];
+		matrix[x][y] = 0
+		snake.push([snake[0][0], snake[0][1] - 1]);
+	}
+	if(direction === 'down-right'){
+		x = snake[0][0];
+		y = snake[0][1];
+		matrix[x][y] = 0
+		snake.push([snake[2][0], snake[2][1] + 1]);
+	}
+	if(direction === 'up-right'){
+		x = snake[0][0];
+		y = snake[0][1];
+		matrix[x][y] = 0
+		snake.push([snake[2][0], snake[2][1] - 1]);
+	}
+}
+
 var snake = [
-	[0, 0],
-	[1, 0],
-	[2, 0],
+	[2, 2],
+	[3, 2],
+	[4, 2],
 ];
 
 setInterval(function() {
 	clearCanvas();
 	var matrix = createMatrix(10, 10);
 	applySnakeToMatrix(snake, matrix);
+	snakeDirection(snake, 'up-right' matrix);
 	drawMatrix(matrix);
 }, 500);
