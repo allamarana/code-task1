0ccaf34ba89031355701557dd513c755e1669583
allamarana <allamarana.t@gmail.com>
2017-07-23 00:12:37 +0300

    correct directions "left", "right" added

--- snake.js
+++ snake.js
@@ -10,56 +10,57 @@ function applySnakeToMatrix(snake, matrix){
 
 }
 
-function snakeDirection(snake, direction, matrix){
-	var x, y;
-	if(direction === 'right'){
-		x = snake[0][0];
-		y = snake[0][1];
-		matrix[x][y] = 0
-		snake.push([snake[2][0] + 1, snake[2][1]]);
+function snakeDirection(snake, direction){
+	var x = snake[0][0];
+	var y = snake[0][1];
+	if(direction === 'right'){		
+		snake.unshift([x + 1, y]);
+		snake.pop();
 	}
 	if(direction === 'left'){
-		x = snake[2][0];
-		y = snake[2][1];
-		matrix[x][y] = 0
-		snake.push([snake[0][0] - 1, snake[0][1]]);
+		snake.push([x - 1, y]);
+		snake.shift();
 	}
-	if(direction === 'down-left'){
-		x = snake[2][0];
-		y = snake[2][1];
-		matrix[x][y] = 0
-		snake.push([snake[0][0], snake[0][1] + 1]);
-	}
-	if(direction === 'up-left'){
-		x = snake[2][0];
-		y = snake[2][1];
-		matrix[x][y] = 0
-		snake.push([snake[0][0], snake[0][1] - 1]);
-	}
-	if(direction === 'down-right'){
-		x = snake[0][0];
-		y = snake[0][1];
-		matrix[x][y] = 0
-		snake.push([snake[2][0], snake[2][1] + 1]);
-	}
-	if(direction === 'up-right'){
-		x = snake[0][0];
-		y = snake[0][1];
-		matrix[x][y] = 0
-		snake.push([snake[2][0], snake[2][1] - 1]);
-	}
+	// if(direction === 'down-left'){
+	// 	x = snake[2][0];
+	// 	y = snake[2][1];
+	// 	matrix[x][y] = 0
+	// 	snake.push([snake[0][0], snake[0][1] + 1]);
+	// }
+	// if(direction === 'up-left'){
+	// 	x = snake[2][0];
+	// 	y = snake[2][1];
+	// 	matrix[x][y] = 0
+	// 	snake.push([snake[0][0], snake[0][1] - 1]);
+	// }
+	// if(direction === 'down-right'){
+	// 	x = snake[0][0];
+	// 	y = snake[0][1];
+	// 	matrix[x][y] = 0
+	// 	snake.push([snake[2][0], snake[2][1] + 1]);
+	// }
+	// if(direction === 'up-right'){
+	// 	x = snake[0][0];
+	// 	y = snake[0][1];
+	// 	matrix[x][y] = 0
+	// 	snake.push([snake[2][0], snake[2][1] - 1]);
+	// }
 }
 
 var snake = [
-	[2, 2],
+	[4, 2], // HEAD
 	[3, 2],
-	[4, 2],
+	[2, 2], // Tail
 ];
 
 setInterval(function() {
 	clearCanvas();
 	var matrix = createMatrix(10, 10);
 	applySnakeToMatrix(snake, matrix);
-	snakeDirection(snake, 'up-right', matrix);
 	drawMatrix(matrix);
+	snakeDirection(snake, 'left', matrix);
 }, 500);
+
+
+
+
