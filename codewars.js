59f97c165efd28e967ecef8691dd9376821dabb5
allamarana <allamarana.t@gmail.com>
2017-08-12 23:49:45 +0300

    object task solved

--- codewars.js
+++ codewars.js
@@ -10,15 +10,19 @@ console.log(sortFromHighest(125463));*/
 
 function cakes(recipe, available) {
   var recProp = Object.keys(recipe);
-  var res = [];
+  //var res = [];
   for (var i = 0; i < recProp.length; i++){
      if (available[recProp[i]] === undefined){
        return 0;
-     } else {
-       if(Math.floor(recipe[recProp[i]] % available[recProp[i]) >= res){
-         res.push(Math.floor(recipe[recProp[i]] % available[recProp[i]));
+     } /*else {
+     	var num = recipe[recProp[i]] % available[recProp[i];
+       if(Math.floor(num) >= res){
+         res.push(Math.floor(num));
        }
      }
   }
-  return res;
+  return res;*/
 }
+
+console.log(cakes(recipe = {apples: 50, flour: 500, sugar: 200, eggs: 1},
+    available = {flour: 1200, sugar: 1200, eggs: 5, milk: 200}))
