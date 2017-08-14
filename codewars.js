8473d7c47f0edb960f400b3584dbae2b311c3fcb
allamarana <allamarana.t@gmail.com>
2017-08-14 23:17:17 +0300

    codewars tasks solved

--- codewars.js
+++ codewars.js
@@ -8,21 +8,38 @@
 
 console.log(sortFromHighest(125463));*/
 
-function cakes(recipe, available) {
+/*function cakes(recipe, available) {
   var recProp = Object.keys(recipe);
-  //var res = [];
+  var min = Infinity;
   for (var i = 0; i < recProp.length; i++){
-     if (available[recProp[i]] === undefined){
+     if (!available[recProp[i]]){
        return 0;
-     } /*else {
-     	var num = recipe[recProp[i]] % available[recProp[i];
-       if(Math.floor(num) >= res){
-         res.push(Math.floor(num));
-       }
+     }
+     var num = Math.floor(available[recProp[i]] / recipe[recProp[i]]);
+     if (min > num) {
+       min = num;
      }
   }
-  return res;*/
+  return min;
 }
 
-console.log(cakes(recipe = {apples: 50, flour: 500, sugar: 200, eggs: 1},
-    available = {flour: 1200, sugar: 1200, eggs: 5, milk: 200}))
+console.log(cakes(recipe = {flour: 500, sugar: 200, eggs: 1};
+    available = {flour: 1200, sugar: 1200, eggs: 5, milk: 200}))*/
+
+    function findOdd(A){
+    	var odd = {};
+    	for (var i = 0; i < A.length; i++) {
+    		if (A[i] in odd){
+    			odd[A[i]]++;
+    		} else {
+    			odd[A[i]] = 1;
+    		}
+    	 }
+    	 for (var key in odd){
+    	 	if(odd[key] % 2 !== 0){
+    	 		return +key
+    	 	}
+    	 }
+    }
+
+    console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]))
