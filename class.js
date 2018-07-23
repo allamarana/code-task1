da02b06b935564e912d9e9115f5b9f823bd82fc8
Lydmila Troyanovska <allamarana@MacBook-Pro-Ivan.local>
2018-07-23 13:00:53 +0300

    class

---
+++ class.js
@@ -0,0 +1,7 @@
+var name = 'Igor ';
+var lastname = 'Petrenko';
+var myname = 'Liudmyla';
+console.log(`
+	Hello, ${name} ${lastname}, I'm glad to offer you our help with your project!
+	your faithfully, ${myname}
+	`);
