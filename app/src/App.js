e104b0e36099c7b7a6ca3bd837ad7925d288b765
Lydmila Troyanovska <allamarana.t@gmail.com>
2018-08-08 17:27:00 +0300

    x - o game added

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { render } from 'react-dom';
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+var nextTurn = "X";
+
+
+function targetChange (event){
+  var target = event.target
+  if (target.innerText != "") return;
+  target.innerText = nextTurn;
+  if (nextTurn == "X"){
+    nextTurn = "O"
+  } else { 
+    nextTurn = "X"
   }
 }
 
-export default App;
+
+
+
+function Article() {
+  const body = <section>body</section>;
+
+
+  return (
+    <div className = "grid-container" >
+
+      <div className = "grid-item" onClick = {targetChange} id = "0-0"> </div>
+      <div className = "grid-item" onClick = {targetChange} id = "0-1"> </div>
+      <div className = "grid-item" onClick = {targetChange} id = "0-2"> </div>
+      <div className = "grid-item" onClick = {targetChange} id = "1-0"> </div>
+      <div className = "grid-item" onClick = {targetChange} id = "1-1"> </div>
+      <div className = "grid-item" onClick = {targetChange} id = "1-2"> </div>
+      <div className = "grid-item" onClick = {targetChange} id = "2-0"> </div>
+      <div className = "grid-item" onClick = {targetChange} id = "2-1"> </div>
+      <div className = "grid-item" onClick = {targetChange} id = "2-2"> </div>
+
+    </div>
+  )
+}
+
+
+function App(){
+  return (
+    <div>
+      <h1>  Крестики- нолики </h1>
+      <Article />
+      </div>
+    
+  )
+}
+render (<App />, document.getElementById('root'))
