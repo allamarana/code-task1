74199fdc0bdda4d4af364701c8eec7763c71d9f3
Lydmila Troyanovska <allamarana.t@gmail.com>
2018-08-15 13:34:14 +0300

    tic tac toe rewrote with classes

--- app/src/App.js
+++ app/src/App.js
@@ -2,14 +2,9 @@ import React from 'react';
 import _ from 'lodash';
 import { render } from 'react-dom';
 import './App.css'
+//import './dropdown'
+//import Dropdown from './dropdown';
 
-
-
-
-function hideGame() {
-  var element = document.getElementById("main-div");
-  // element.classList.add("dynamic-style");
-}
 
 class TicTacToe extends React.Component {
   constructor() {
@@ -29,92 +24,88 @@ class TicTacToe extends React.Component {
     var target = event.target
     var id = target.id.split('-');
     if (this.state.matrix[id[0]][id[1]] !== -1) return;
-  
-    // target.innerText = nextTurn;
+    if (this.state.isGameOver) return;
+
+    const currentTurn = 'X' === this.state.nextTurn ? 1 : 0;
+
+    const newState = _.cloneDeep(this.state);
     if (this.state.nextTurn == "X"){
-      const newState = _.cloneDeep(this.state);
       newState.nextTurn = "O";
       newState.matrix[id[0]][id[1]] = 1;
-      this.setState(newState);
-  
-      if(this.state.matrix[0][0] === this.state.matrix[1][0] && this.state.matrix[1][0]  === this.state.matrix[2][0] && this.state.matrix[2][0] === 1) {
-        return this.this.gameOver();
-      } else if (this.state.matrix[0][1] === this.state.matrix[1][1] && this.state.matrix[1][1] === this.state.matrix[2][1] && this.state.matrix[2][1] === 1) {
-        return this.gameOver();
-      } else if (this.state.matrix[0][2] === this.state.matrix[1][2] && this.state.matrix[1][2] === this.state.matrix[2][2] && this.state.matrix[2][2] === 1) {
-        return this.gameOver();
-      } 
-      
-      else if (this.state.matrix[0][0] === this.state.matrix[0][1] && this.state.matrix[0][1] === this.state.matrix[0][2] && this.state.matrix[0][2] === 1) {
-        return this.gameOver();
-      } else if (this.state.matrix[1][0] === this.state.matrix[1][1] && this.state.matrix[1][1] === this.state.matrix[1][2] && this.state.matrix[1][2] === 1) {
-        return this.gameOver();
-      } else if (this.state.matrix[2][0] === this.state.matrix[2][1] && this.state.matrix[2][1] === this.state.matrix[2][2] && this.state.matrix[2][2] === 1) {
-        return this.gameOver();
-      }
-  
-      else if (this.state.matrix[0][0] === this.state.matrix[1][1] && this.state.matrix[1][1] === this.state.matrix[2][2] && this.state.matrix[2][2] === 1) {
-        return this.gameOver();
-      }else if (this.state.matrix[0][2] === this.state.matrix[1][1] && this.state.matrix[1][1] === this.state.matrix[2][0] && this.state.matrix[2][0]  === 1) {
-        return this.gameOver();
-      }
-  
     } else {
-      const newState = _.cloneDeep(this.state);
       newState.nextTurn = "X";
       newState.matrix[id[0]][id[1]] = 0;
-      this.setState(newState);
-
-  
-      if (this.state.matrix[0][0] === this.state.matrix[1][0] && this.state.matrix[1][0] === this.state.matrix[2][0] && this.state.matrix[2][0] === 0) {
-        return this.gameOver();
-      } else if (this.state.matrix[0][1] === this.state.matrix[1][1] && this.state.matrix[1][1] === this.state.matrix[2][1] && this.state.matrix[2][1] === 0) {
-        return this.gameOver();
-      } else if (this.state.matrix[0][1] === this.state.matrix[1][1] && this.state.matrix[1][1] === this.state.matrix[2][1] && this.state.matrix[2][1] === 0) {
-        return this.gameOver();
-      } 
-      
-      else if (this.state.matrix[0][0] === this.state.matrix[0][1] && this.state.matrix[0][1] === this.state.matrix[0][2] && this.state.matrix[0][2] === 0) {
-        return this.gameOver();
-      } else if (this.state.matrix[1][0] === this.state.matrix[1][1] && this.state.matrix[1][1] === this.state.matrix[1][2] && this.state.matrix[1][2] === 0) {
-        return this.gameOver();
-      } else if (this.state.matrix[2][0] === this.state.matrix[2][1] && this.state.matrix[2][1] === this.state.matrix[2][2] && this.state.matrix[2][2] === 0) {
-        return this.gameOver();
-      }
-  
-      else if (this.state.matrix[0][0] === this.state.matrix[1][1] && this.state.matrix[1][1] === this.state.matrix[2][2] && this.state.matrix[2][2] === 0) {
-        return this.gameOver();
-      }else if (this.state.matrix[0][2] === this.state.matrix[1][1] && this.state.matrix[1][1] === this.state.matrix[2][0] && this.state.matrix[2][0] === 0) {
-        return this.gameOver();
-      }
     }
-    console.log(this.state.matrix);
+    
+    this.setState(function(state) {
+      return {
+        ...state,
+        matrix: newState.matrix,
+        nextTurn: newState.nextTurn,
+      };
+    });
+  
+    if(newState.matrix[0][0] === currentTurn && newState.matrix[1][0]  === currentTurn && newState.matrix[2][0] === currentTurn) {
+      return this.gameOver();
+    } 
+    if (newState.matrix[0][1] === currentTurn && newState.matrix[1][1] === currentTurn && newState.matrix[2][1] === currentTurn) {
+      return this.gameOver();
+    }
+    if (newState.matrix[0][2] === currentTurn && newState.matrix[1][2] === currentTurn && newState.matrix[2][2] === currentTurn) {
+      return this.gameOver();
+    } 
+    
+    if (newState.matrix[0][0] === currentTurn && newState.matrix[0][1] === currentTurn && newState.matrix[0][2] === currentTurn) {
+      return this.gameOver();
+    }
+    if (newState.matrix[1][0] === currentTurn && newState.matrix[1][1] === currentTurn && newState.matrix[1][2] === currentTurn) {
+      return this.gameOver();
+    }
+    if (newState.matrix[2][0] === currentTurn && newState.matrix[2][1] === currentTurn && newState.matrix[2][2] === currentTurn) {
+      return this.gameOver();
+    }
+
+    if (newState.matrix[0][0] === currentTurn && newState.matrix[1][1] === currentTurn && newState.matrix[2][2] === currentTurn) {
+      return this.gameOver();
+    }
+    if (newState.matrix[0][2] === currentTurn && newState.matrix[1][1] === currentTurn && newState.matrix[2][0]  === currentTurn) {
+      return this.gameOver();
+    }
+
+    console.log(newState);
   }
 
   gameOver() {
-    this.state.isGameOver = true;
+    this.setState(function(state) {
+      return {
+        ...state,
+        isGameOver: true,
+      };
+    });
     console.log('game over');
-    hideGame();
   }
 
 
   render() {
-    console.log(this.state.matrix);
     return (
-      <div className = "grid-container" id = "main-div" >
-        {}
-        <div className="grid-item" onClick={this.click.bind(this)} id="0-0">{this.state.matrix[0][0] === -1 ? "" : (this.state.matrix[0][0] === 1 ? "X" : "O")}</div>
-        <div className="grid-item" onClick={this.click.bind(this)} id="0-1">{this.state.matrix[0][1] === -1 ? "" : (this.state.matrix[0][1] === 1 ? "X" : "O")}</div>
-        <div className="grid-item" onClick={this.click.bind(this)} id="0-2">{this.state.matrix[0][2] === -1 ? "" : (this.state.matrix[0][2] === 1 ? "X" : "O")}</div>
+      <div>
+        <div className = "grid-container" id = "main-div">
+          <div className="grid-item" onClick={this.click.bind(this)} id="0-0">{this.state.matrix[0][0] === -1 ? "" : (this.state.matrix[0][0] === 1 ? "X" : "O")}</div>
+          <div className="grid-item" onClick={this.click.bind(this)} id="0-1">{this.state.matrix[0][1] === -1 ? "" : (this.state.matrix[0][1] === 1 ? "X" : "O")}</div>
+          <div className="grid-item" onClick={this.click.bind(this)} id="0-2">{this.state.matrix[0][2] === -1 ? "" : (this.state.matrix[0][2] === 1 ? "X" : "O")}</div>
 
-        <div className="grid-item" onClick={this.click.bind(this)} id="1-0">{this.state.matrix[1][0] === -1 ? "" : (this.state.matrix[1][0] === 1 ? "X" : "O")}</div>
-        <div className="grid-item" onClick={this.click.bind(this)} id="1-1">{this.state.matrix[1][1] === -1 ? "" : (this.state.matrix[1][1] === 1 ? "X" : "O")}</div>
-        <div className="grid-item" onClick={this.click.bind(this)} id="1-2"> {this.state.matrix[1][2] === -1 ? "" : (this.state.matrix[1][2] === 1 ? "X" : "O")} </div>
+          <div className="grid-item" onClick={this.click.bind(this)} id="1-0">{this.state.matrix[1][0] === -1 ? "" : (this.state.matrix[1][0] === 1 ? "X" : "O")}</div>
+          <div className="grid-item" onClick={this.click.bind(this)} id="1-1">{this.state.matrix[1][1] === -1 ? "" : (this.state.matrix[1][1] === 1 ? "X" : "O")}</div>
+          <div className="grid-item" onClick={this.click.bind(this)} id="1-2"> {this.state.matrix[1][2] === -1 ? "" : (this.state.matrix[1][2] === 1 ? "X" : "O")} </div>
 
-        <div className="grid-item" onClick={this.click.bind(this)} id="2-0"> {this.state.matrix[2][0] === -1 ? "" : (this.state.matrix[2][0] === 1 ? "X" : "O")}</div>
-        <div className="grid-item" onClick={this.click.bind(this)} id="2-1"> {this.state.matrix[2][1] === -1 ? "" : (this.state.matrix[2][1] === 1 ? "X" : "O")}</div>
-        <div className="grid-item" onClick={this.click.bind(this)} id="2-2"> {this.state.matrix[2][2] === -1 ? "" : (this.state.matrix[2][2] === 1 ? "X" : "O")}</div>
+          <div className="grid-item" onClick={this.click.bind(this)} id="2-0"> {this.state.matrix[2][0] === -1 ? "" : (this.state.matrix[2][0] === 1 ? "X" : "O")}</div>
+          <div className="grid-item" onClick={this.click.bind(this)} id="2-1"> {this.state.matrix[2][1] === -1 ? "" : (this.state.matrix[2][1] === 1 ? "X" : "O")}</div>
+          <div className="grid-item" onClick={this.click.bind(this)} id="2-2"> {this.state.matrix[2][2] === -1 ? "" : (this.state.matrix[2][2] === 1 ? "X" : "O")}</div>
 
+        </div>
+        <div className={this.state.isGameOver ? '' : 'hide'}>
+          Победили {this.state.nextTurn === 'X' ? 'O' : 'X'}
+        </div>
       </div>
     )
   }
@@ -123,10 +114,14 @@ class TicTacToe extends React.Component {
 
 function App(){
   return (
-    <div>
-      <h1>  Крестики- нолики </h1>
+   <div className="mainDiv">
+       <h1>  Крестики- нолики </h1>
       <TicTacToe />
       </div>
+
+     /*<div>
+       <Dropdown />
+    </div>*/
     
   )
 }
--- app/src/dropdown.js
+++ app/src/dropdown.js
@@ -1,3 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+class Dropdown extends React.Component {
+    constructor(){
+        super();
+        this.state = { isOpened: false };
+        
+    }
+    toggleState(){
+        this.setState({ isOpened: !this.state.isOpened })
+    }
+    render(){
+        console.log('isOpened', this.state.isOpened)
+        return <div onClick = {this.toggleState.bind(this)}> Dropdown down down done! </div>
+    }
+}
+
+export default Dropdown;
